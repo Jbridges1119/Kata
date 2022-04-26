475b91b279957538bca34b6aa94434e3e43cd1c1
Jeff Bridges <jbridges1119@gmail.com>
2022-04-26 22:57:49 +0000

    lighthouse kata 7

---
+++ kata7.js
@@ -0,0 +1,27 @@
+const checkAir = function (samples, threshold) {
+  let dirty = 0;
+  for (let i = 0; i < samples.length; i++) {
+    if (samples[i] === 'dirty') {
+      dirty++;
+    }
+  } dirty /= samples.length;
+  if (threshold > dirty) {
+    return "Clean";
+  } else {
+    return "Polluted";
+  }}; 
+
+console.log(checkAir(
+  ['clean', 'clean', 'dirty', 'clean', 'dirty', 'clean', 'clean', 'dirty', 'clean', 'dirty'],
+  0.3
+));
+
+console.log(checkAir(
+  ['dirty', 'dirty', 'dirty', 'dirty', 'clean'],
+  0.25
+));
+
+console.log(checkAir(
+  ['clean', 'dirty', 'clean', 'dirty', 'clean', 'dirty', 'clean'],
+  0.9
+))
