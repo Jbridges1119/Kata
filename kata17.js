55e53e90772897c9a1468199d1004ccb51b01a1a
Jeff Bridges <jbridges1119@gmail.com>
2022-05-26 17:43:34 +0000

    Kata 17

--- kata17.js
+++ kata17.js
@@ -1,27 +1,14 @@
 const urlDecode = function (text) {
-  let obj1 = "";
-  let obj2 = "";
-  let result = {};
-  for (let i = 0; i < text.length; i++) {
-    if (text[i] === "%" && text[i + 1] === "2" && text[i + 2] === "0") {
-      obj1 += " ";
-      i += 2
-    } else if (text[i] === "=") {
-      obj2 = obj1;
-      obj1 = "";
-    } else if (text[i] === "&") {
-      result[obj2] = obj1;
-      obj1 = "";
-      obj2 = "";
-    } else {
-      obj1 += text[i];
-    }
-  } result[obj2] = obj1;
-  return result;
-};
+  let newStrn = text.replace(/%20/g, " ").split("&",)
+  let spltStrn = [];
+  let newObj = {};
 
+  for (let i = 0; i < newStrn.length; i++) {
+      spltStrn.push(newStrn[i].split("="));
+      newObj[spltStrn[i][0]] = spltStrn[i][1];
+  } return newObj;
+}
 console.log(urlDecode("duck=rubber"));
 console.log(urlDecode("bootcamp=Lighthouse%20Labs"));
 console.log(urlDecode("city=Vancouver&weather=lots%20of%20rain"));
 console.log(urlDecode("city=Vancouver&weather=lots%20of%20rain").weather);
-
